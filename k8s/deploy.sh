d4d2f615482026f5bac32e53d18649837c53b1b8
Shachar297 <thisishachar@gmail.com>
2022-07-30 12:49:41 +0300

    commit

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,4 +1,4 @@
-minikube start --insecure-registries="host.minikube.internal:8083"
+minikube start --insecure-registry="host.minikube.internal:8083"
 minikine addons enable registriy
 minikube addons configure registry-creds #1. no 2.no 3.y 4. host.minikube.internal:8083 5. n
 
--- k8s/mysql/scripts/init-db.sh
+++ k8s/mysql/scripts/init-db.sh
@@ -7,21 +7,21 @@ echo "Crating database "$DB_NAME""
 
 sleep 10
 
-echo "CREATE DATABASE "$DB_NAME";" | docker exec -u 0 -i $mysql_pod mysql -u root -p123456
+echo "CREATE DATABASE "$DB_NAME";" | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456
 
 echo "Crating tables..."
 
 sleep 5 
 
-cat home_apartments.sql     | docker exec -u 0 -i $mysql_pod mysql -u root -p123456 "$DB_NAME"
-cat home_buy_items.sql      | docker exec -u 0 -i $mysql_pod mysql -u root -p123456 "$DB_NAME"
-cat home_categories.sql     | docker exec -u 0 -i $mysql_pod mysql -u root -p123456 "$DB_NAME"
-cat home_shopping_list.sql  | docker exec -u 0 -i $mysql_pod mysql -u root -p123456 "$DB_NAME"
-cat home_users.sql          | docker exec -u 0 -i $mysql_pod mysql -u root -p123456 "$DB_NAME"
-cat home_fixes.sql          | docker exec -u 0 -i $mysql_pod mysql -u root -p123456 "$DB_NAME"
+cat home_apartments.sql     | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456 "$DB_NAME"
+cat home_buy_items.sql      | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456 "$DB_NAME"
+cat home_categories.sql     | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456 "$DB_NAME"
+cat home_shopping_list.sql  | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456 "$DB_NAME"
+cat home_users.sql          | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456 "$DB_NAME"
+cat home_fixes.sql          | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456 "$DB_NAME"
 
 echo "Testing database initialization..."
 
 sleep 5
 
-echo "SELECT * FROM home.users" | docker exec -u 0 -i $MYSQL_CONTAINER_ID mysql -u root -p123456 "$DB_NAME"
+echo "SELECT * FROM home.users" | kubectl exec -it $mysql_pod -n mysql -- mysql -u root -p123456 "$DB_NAME"
